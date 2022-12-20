8ea8942cd7f214b2e110477d740be7b35dc4d25b
w0pro <ya.sapronov@gmail.com>
2022-12-20 03:47:12 +0300

    init: add new methods

--- app/start.js
+++ app/start.js
@@ -1,15 +1,19 @@
 import * as readline from 'node:readline';
 import { stdin as input, stdout as output } from 'node:process';
-// import * as path from 'node:path';
+import * as path from 'node:path';
 import * as os from 'os';
 import fs from 'node:fs/promises';
 import { existsSync } from 'node:fs';
-import {readdir} from 'node:fs/promises'
+import {readdir} from 'node:fs/promises';
+import {createReadStream} from 'node:fs'
 
 const arrArgv = process.argv
 let name = '';
 const homeDir = os.homedir();
 let actualDir = '';
+
+const sep = path.sep;
+
 
 const args = () => {
     arrArgv.forEach((arg) => {
@@ -26,42 +30,67 @@ args();
 
 
 const fileMethods = {
-    actD: actualDir,
-    add: async (param)  => {
-     await fs.writeFile(`${actualDir}/${param[0]}`, '');
-    },
     cd:async (param) => {
-       if (existsSync(`${actualDir}/${param[0]}`)) {
-         actualDir +=`/${param}`;
+        let prm = '';
+        if (param[0][0] === `${sep}`) {
+            prm = param[0].substring(1);
+        }
+       if (existsSync(`${actualDir}${sep}${prm}`)) {
+         actualDir +=`${sep}${prm}`;
         } else {
            throw Error('Path not exist')
         }
 
     },
     ls: async () => {
-        const arr = []
+        const arrFile = [];
+        const arrDirectory = [];
         const files = await readdir(`${actualDir}`);
         for await (let file of files) {
-            if ((await fs.lstat(`${actualDir}/${file}`)).isDirectory()) {
-                arr.push({name: file, value: 'Directory'})
+            if ((await fs.lstat(`${actualDir}${sep}${file}`)).isDirectory()) {
+                arrDirectory.push({name: file, value: 'Directory'})
             } else {
-                arr.push({name: file, value: 'File'})
+                arrFile.push({name: file, value: 'File'})
             }
-
-
         }
-        console.table(arr)
+        arrDirectory.sort((a, b) => a.name > b.name ? 1 : -1)
+        arrFile.sort((a, b) => a.name > b.name ? 1 : -1)
+
+
+        console.table(arrDirectory.concat(arrFile))
     },
     up: async () => {
         if (actualDir === homeDir) {
             throw Error('You are in the root folder')
         } else {
-            let dirs = actualDir.split('/').slice(0, -1).join('/');
+            let dirs = actualDir.split(`${sep}`).slice(0, -1).join(`${sep}`);
             if (existsSync(dirs)) {
                 actualDir = dirs
             }
 
         }
+    },
+    cat: async (param) => {
+        if (existsSync(`${actualDir}${sep}${param[0]}`)) {
+            const read = createReadStream(`${actualDir}${sep}${param[0]}`)
+            read.pipe(output)
+        } else {
+            throw new Error('File not exit')
+        }
+    },
+    add: async (param) => {
+        if (existsSync(`${actualDir}${sep}${param[0]}`)) {
+            throw new Error('File exist!')
+        } else {
+            await fs.writeFile(`${actualDir}${sep}${param[0]}`, `${param[1]}`);
+        }
+    },
+    rn: async (param) => {
+        if (!existsSync(`${actualDir}${sep}${param[0]}`) || existsSync(`${actualDir}${sep}${param[1]}`)) {
+            throw new Error('FS operation failed')
+        }
+
+        fs.rename(`${actualDir}${sep}${param[0]}`, `${actualDir}${sep}${param[1]}`);
     }
 }
 
@@ -75,10 +104,10 @@ rl.on('line', (input) => {
    if (fileMethods[command]) {
        fileMethods[command](arrInp.slice(1))
            .then(res => {
-               if (res) {
+
                    console.log('Operation complete!')
                    console.log(actualDir)
-               }
+
 
            })
            .catch(er => console.log(er.message))
